5c50c8502f8807adcfd59a8c88469f59102ba217
dddmogui13 <dmytropotorocha213@gmail.com>
2023-10-01 13:00:16 +0300

    ddd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+ 
+gallery.addEventListener('click', onGalleryItemClick);
+
+ 
+function onGalleryItemClick(event) {
+    event.preventDefault();  
+ 
+    if (event.target.tagName === 'IMG') {
+         
+        const largeImageUrl = event.target.dataset.source;
+
+        // Відкрийте модальне вікно з великим зображенням
+        const instance = basicLightbox.create(`
+            <img src="${largeImageUrl}" width="800" height="600">
+        `);
+
+        instance.show();
+    }
+}
