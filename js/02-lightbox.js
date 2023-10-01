d2422c808ac66f7652199168a9ef4fcc0c904771
dddmogui13 <dmytropotorocha213@gmail.com>
2023-10-01 14:55:58 +0300

    ddd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const textImg = galleryItems
+    .map(
+        (obj) =>
+            `<li class="gallery__item">
+               <a class="gallery__link" href="${obj.original}">
+                  <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+               </a>
+            </li>`
+    )
+    .join(" ");
+
+list.insertAdjacentHTML("afterbegin", textImg);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox", function () {
+    console.log('Lightbox opened');
+});
